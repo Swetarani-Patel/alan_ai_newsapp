885df957e3051f9cf8fcb6bc5fce81d1f0374916
Swetarani Patel <sweta@Swetaranis-MacBook-Air.local>
2023-12-02 08:57:34 +0530

    initial commit for alan ai news app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
-
+import alanBtn from '@alan-ai/alan-sdk-web';
+const alanKey = '6ce25c905a2c8e5870949d9710e0dbe32e956eca572e1d8b807a3e2338fdd0dc/stage';
 function App() {
+  useEffect(()=>{
+    alanBtn({
+      key:alanKey,
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Alan AI News Application</h1>
     </div>
   );
 }
